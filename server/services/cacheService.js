d9ee831e13b422013bb281d97ab6f5f9cee75b76
Suraj <suraj@gmail.com>
2026-05-22 16:26:34 +0530

    fix cache service

--- server/services/cacheService.js
+++ server/services/cacheService.js
@@ -1,16 +1,81 @@
-const NodeCache =
-require('node-cache');
+const NodeCache = require('node-cache');
 
+// Cache TTL = 10 min
 const cache =
 new NodeCache({
 
 stdTTL:
-120,
+600,
 
 checkperiod:
-150
+120
 
 });
 
-module.exports =
-cache;
+
+
+// GET
+
+const getCache =
+(key)=>{
+
+return cache.get(key);
+
+};
+
+
+
+// SET
+
+const setCache =
+(
+key,
+value
+)=>{
+
+cache.set(
+key,
+value
+);
+
+};
+
+
+
+// CLEAR SINGLE
+
+const deleteCache =
+(
+key
+)=>{
+
+cache.del(
+key
+);
+
+};
+
+
+
+// CLEAR ALL
+
+const clearCache =
+()=>{
+
+cache.flushAll();
+
+};
+
+
+
+module.exports={
+
+getCache,
+
+setCache,
+
+deleteCache,
+
+clearCache
+
+};
